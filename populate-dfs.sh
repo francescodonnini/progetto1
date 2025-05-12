662f97f6a09e15f991feb1960313a529826160c8
Francesco Donnini <donnini.francesco00@gmail.com>
2025-05-12 21:51:53 +0200

    aggiunto Dockerfile per NiFi che aggiunge librerie per Hadoop, lo script populate-dfs.sh crea alcune directory su cui è possibile salvare i risultati

--- populate-dfs.sh
+++ populate-dfs.sh
@@ -1,1 +1,5 @@
-hdfs dfs -put /opt/dataset/data.csv /data.csv
+docker compose exec namenode hdfs dfs -mkdir /user
+docker compose exec namenode hdfs dfs -mkdir /user/spark
+docker compose exec namenode hdfs dfs -chown spark:hadopp /user/spark
+docker compose exec namenode hdfs dfs -chmod 755 /user/spark
+docker compose exec namenode hdfs dfs -put /opt/dataset/data.csv /data.csv
