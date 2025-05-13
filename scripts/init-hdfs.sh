f545e5bf4731c180cb0ac06e67be10b0230ead58
Francesco Donnini <donnini.francesco00@gmail.com>
2025-05-14 00:05:47 +0200

    aggiunto script per inizializzare cartelle hdfs

--- scripts/init-hdfs.sh
+++ scripts/init-hdfs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Wait for NameNode web UI to be up (port 9870)
-echo "Waiting for NameNode to be ready..."
+echo "waiting for 'namenode' to be ready..."
 until curl -s http://namenode:9870 >/dev/null; do
   sleep 3
 done
 
-# Wait for the HDFS command to succeed (e.g., `hdfs dfs -ls /`)
 until hdfs dfs -ls / >/dev/null; do
   echo "HDFS not ready yet..."
   sleep 6
 done
 
 # Create HDFS directories
-echo "Creating initial HDFS directories..."
+echo "creating directories for spark cluster"
 hdfs dfs -mkdir /user
 hdfs dfs -mkdir /user/spark
 hdfs dfs -chown spark:hadopp /user/spark
 hdfs dfs -chmod 755 /user/spark
+hdfs dfs -put /opt/dataset/data.csv /data.csv
+echo "directories created successfully"
