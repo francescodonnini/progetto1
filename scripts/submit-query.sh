860cf7bde333e840628306df39101293700e5258
Francesco Donnini <donnini.francesco00@gmail.com>
2025-05-29 00:34:33 +0200

    aggiornato submit query

--- scripts/submit-query.sh
+++ scripts/submit-query.sh
@@ -1,5 +1,5 @@
-if [ "$#" -ne 2 ]; then
-  echo "usage: $0 <query> <api>"
+if [ "$#" -ne 3 ] && [ "$#" -ne 5 ]; then
+  echo "usage: $0 --query <query> (-R|-D) [--time <runs>]"
   exit 1
 fi
-docker compose exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client --class io.github.francescodonnini.QueryDispatcher /opt/app/query.jar $1 $2
+docker compose exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client --class io.github.francescodonnini.QueryDispatcher /opt/app/query.jar $@
