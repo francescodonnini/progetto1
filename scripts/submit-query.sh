927c4ea862d508dbe62cce3df24b82114d013e39
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-01 23:08:35 +0200

    aggiunta opzione per leggere file in locale (da spark)

--- scripts/submit-query.sh
+++ scripts/submit-query.sh
@@ -1,5 +1,1 @@
-if [ "$#" -ne 3 ] && [ "$#" -ne 5 ]; then
-  echo "usage: $0 --query <query> (-R|-D) [--time <runs>]"
-  exit 1
-fi
 docker compose exec spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client --class io.github.francescodonnini.QueryDispatcher /opt/app/query.jar $@
