9258f472462d2b2672b28e7ae49f073f7e2ad74f
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-08 15:50:58 +0200

    aggiunti link per electricitymap.com, test server si spegne se si usano i link di electricitymap.com

--- http-server/server.py
+++ http-server/server.py
@@ -1,10 +1,22 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+import logging
+import os
+import sys
 
 class CSVRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         return super().do_GET()
 
+def get_bool_env(name):
+    v = os.environ.get(name)
+    if v is None:
+        return False
+    return v.lower() in (1, 'true', 't')
+
 if __name__ == "__main__":
+    use_test_server = get_bool_env('USE_TEST_SERVER')
+    if not use_test_server:
+        sys.exit(0)
     server = HTTPServer(('0.0.0.0', 8080), CSVRequestHandler)
-    print("Serving on http://0.0.0.0:8080")
+    logging.info("serving on http://0.0.0.0:8080")
     server.serve_forever()
