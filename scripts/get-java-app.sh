d3e9085d576e68e86f3c03029176d790f65f0a82
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-09 20:36:31 +0200

    aggiunto flag f per la rimozione della cartella che contiene l'app Java

--- scripts/get-java-app.sh
+++ scripts/get-java-app.sh
@@ -1,7 +1,6 @@
 mkdir -p target
 git clone https://github.com/francescodonnini/query.git tmp-query
 cd tmp-query
-mvn clean package
-cp ./target/uber-jar-with-dependencies.jar ../target/
+mvn clean package && cp ./target/uber-jar-with-dependencies.jar ../target/
 cd ..
-rm -r tmp-query
+rm -rf tmp-query
