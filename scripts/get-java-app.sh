740b73863a865293fe63d29cff4327b9a7855c32
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-09 19:59:25 +0200

    aggiunto README.md

---
+++ scripts/get-java-app.sh
@@ -0,0 +1,7 @@
+mkdir -p target
+git clone https://github.com/francescodonnini/query.git tmp-query
+cd tmp-query
+mvn clean package
+cp ./target/uber-jar-with-dependencies.jar ../target/
+cd ..
+rm -r tmp-query
